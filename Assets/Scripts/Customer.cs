cbb704bceea4c1d45a5ac861c0ee5605bf2edb00
Chris Anzilotti <anzilottichr@gmail.com>
2026-03-30 12:52:43 -0400

    Prefabs

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -5,7 +5,7 @@ public class Customer : MonoBehaviour
 {
     [Header("Customer Patience Levels")]
     [SerializeField] private TextMeshProUGUI patienceText;
-    [SerializeField] private float patience;
+    [SerializeField] private float patience = 20f;
     
-    
+
 }
